e40f9c646259c0378ffa09ccb8cfe71488d5784d
vytautas-dotcom <tumas.vytautas@gmail.com>
2021-12-09 16:10:07 +0200

    Normalize data

--- LogisticRegression/LogisticRegression/Display.cs
+++ LogisticRegression/LogisticRegression/Display.cs
@@ -11,13 +11,20 @@ namespace LogisticRegression
         public void Show()
         {
             Data data = new Data();
-
+            double[][] rawData = data.data;
             Console.WriteLine("\nRaw data: \n");
             Console.WriteLine("      Age    Sex  Kidney Died");
             Console.WriteLine("=======================================");
-            ShowData(data.data, 5, 2, true);
-            
-            
+            ShowData(rawData, 5, 2, true);
+
+            Console.WriteLine("Normalizing age and kidney data");
+            int[] columns = new int[] { 0, 2 };
+            Normalize(rawData, columns);
+            Console.WriteLine("Done");
+            Console.WriteLine("\nNormalized data: \n");
+            Console.WriteLine("      Age    Sex  Kidney Died");
+            Console.WriteLine("=======================================");
+            ShowData(rawData, 5, 2, true);
         }
         static void ShowData(double[][] data, int numRows, int decimals, bool indices)
         {
@@ -46,6 +53,52 @@ namespace LogisticRegression
                 Console.Write(v.ToString("F" + decimals) + " ");
             }
             Console.WriteLine("\n");
+
+        }
+
+        static double[][] Normalize(double[][] rawData, int[] columns)
+        {
+            int rows = rawData.Length;
+            int cols = rawData[0].Length;
+
+            double[][] Mean_STD = new double[2][];
+            double[][] normalizedData = new double[rawData.Length][];
+
+            for (int i = 0; i < 2; i++)
+            {
+                Mean_STD[i] = new double[cols];
+            }
+            //mean and standard deviation for each column
+            for (int i = 0; i < cols; i++)
+            {
+                double sum = 0.0;
+                for (int j = 0; j < rows; j++)
+                {
+                    sum += rawData[j][i];
+                }
+                double mean = sum / rows;
+                Mean_STD[0][i] = mean;
+
+                double sumSquares = 0.0;
+                for (int k = 0; k < rows; k++)
+                {
+                    sumSquares += (rawData[k][i] - mean) * (rawData[k][i] - mean);
+                }
+                double stdDev = Math.Sqrt(sumSquares / rows);
+                Mean_STD[1][i] = stdDev;
+            }
+            //normalize
+            for (int i = 0; i < columns.Length; i++)
+            {
+                int column = columns[i];
+                double mean = Mean_STD[0][column];
+                double stdDev = Mean_STD[1][column];
+                for (int j = 0; j < rows; j++)
+                {
+                    rawData[j][column] = (rawData[j][column] - mean) / stdDev;
+                }
+            }
+            return Mean_STD;
         }
     }
 }
